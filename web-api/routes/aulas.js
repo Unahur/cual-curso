2bc4b81cb35991686fb5d5676d685a9e24be8790
jonathanfreiredasilva <jonathan.agustin.fds@gmail.com>
2019-09-12 05:53:47 -0300

    small bugs fixed

--- web-api/routes/aulas.js
+++ web-api/routes/aulas.js
@@ -15,7 +15,7 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
   models.aula
     .create({ edificio: req.body.edificio, numero_aula: req.body.numero_aula })
-    .then(aula => res.status(201).send({ id: aula.id, edificio: aula.edificio, numero_aula: numero_aula }))
+    .then(aula => res.status(201).send({ id: aula.id, edificio: aula.edificio, numero_aula: aula.numero_aula }))
     .catch(() => res.sendStatus(500));
 });
 
@@ -23,7 +23,7 @@ const findAula= (id, { onSuccess, onNotFound, onError }) => {
   models.aula
     .findOne({
       attributes: ["id", "edificio"],
-      where: { id, edificio }
+      where: { id}
     })
     .then(aula => (aula ? onSuccess(aula) : onNotFound()))
     .catch(() => onError());
@@ -50,6 +50,17 @@ router.delete("/:id", (req, res) => {
   });
 });
 
-
+router.put("/:id", (req, res) => {
+  const onSuccess = aula =>
+    aula
+      .update({ edificio: req.body.edificio, numero_aula: req.body.numero_aula }, { fields: ["edificio","numero_aula"] })
+      .then(() => res.sendStatus(200))
+      .catch(() => res.sendStatus(500));
+  findAula(req.params.id, {
+    onSuccess,
+    onNotFound: () => res.sendStatus(404),
+    onError: () => res.sendStatus(500)
+  });
+});
 
 module.exports = router;
