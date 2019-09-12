64e0c02658a3bd240b6d59326cff9fe95e61b2a5
IDMorrison <ariel.sadran@hotmail.com>
2019-09-12 09:38:11 -0300

    creo modelo cursada

---
+++ web-api/models/cursada.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const cursada = sequelize.define('cursada', {
+    nombre: DataTypes.STRING
+  }, {});
+  cursada.associate = function(models) {
+    // associations can be defined here
+  };
+  return cursada;
+};
