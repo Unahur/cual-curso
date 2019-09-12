cd06a1ad85f99c1c06cbe7921ceba30303f4d63b
marianraven <mariangsdh@gmail.com>
2019-09-12 09:28:05 -0300

    estudianteRouter

--- web-api/app.js
+++ web-api/app.js
@@ -3,10 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-//tengo pendiente agregar estudiantes en esta pagina o crear una appEstudiantes.js paralela a esta.consultar
+//cree el routerEstudiante.consultar
 var indexRouter = require('./routes/index');
 var examplesRouter = require('./routes/examples');
 
+var estudiantesRouter = require('./routes/estudiantes');
 var app = express();
 
 // view engine setup
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/examples', examplesRouter);
 
+app.use('/estudiantes', estudiantesRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
