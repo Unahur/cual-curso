5477d0fe08d4165ee2cd44ea193805429fcf2f0a
cristians775 <cristians775>
2019-09-26 20:20:49 -0300

    fixing the post method

--- web-client/js/app.js
+++ web-client/js/app.js
@@ -1,17 +1,19 @@
 var url = 'http://localhost:3001/aulas';
+var formulario = document.getElementById('formCreate');
 
 
-
-var formulario = document.getElementById("formCreate");
 formulario.addEventListener('submit', function (evt) {
-    evt.preventDefault(); 
+    evt.preventDefault();
+
+
     var datos = new FormData(formulario);
-    console.log({edificio: datos.get('edificio'),
-    numero_aula: datos.get('numero_aula')     },);
+    var data = {
+        edificio: datos.get('edificio'),
+        numero_aula: datos.get('numero_aula')
+    }
     fetch(url, {
             method: 'POST', // or 'PUT'
-            body: {edificio: datos.get('edificio'),
-                   numero_aula: datos.get('numero_aula')     },// data can be `string` or {object}!
+            body: JSON.stringify(data), // data can be `string` or {object}!
             headers: {
                 'Content-Type': 'application/json'
             }
