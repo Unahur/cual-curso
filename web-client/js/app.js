978c129099d5a4f4cb7ce0d286ddc357d8a1957c
cristians775 <cristiansaldivia775@gmail.com>
2019-09-26 16:48:16 -0300

    add function app.js

--- web-client/js/app.js
+++ web-client/js/app.js
@@ -1,1 +1,22 @@
-const funcion= ()=>¨
+var url = 'http://localhost:3001/aulas';
+
+
+
+var formulario = document.getElementById("formCreate");
+formulario.addEventListener('submit', function (evt) {
+    evt.preventDefault(); 
+    var datos = new FormData(formulario);
+    console.log({edificio: datos.get('edificio'),
+    numero_aula: datos.get('numero_aula')     },);
+    fetch(url, {
+            method: 'POST', // or 'PUT'
+            body: {edificio: datos.get('edificio'),
+                   numero_aula: datos.get('numero_aula')     },// data can be `string` or {object}!
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => console.log('Success:', response));
+
+});
