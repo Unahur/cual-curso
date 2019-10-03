168a8053be284ec0465782507ed7eb712de080f2
Agustin Remonda <42420863+DemonMurderer@users.noreply.github.com>
2019-10-03 02:11:56 -0300

    post agregado

--- web-client/javascript/materia-create.js
+++ web-client/javascript/materia-create.js
@@ -1,11 +1,39 @@
-function obtenerFormulario(){
-    var materia = document.getElementById("materia").value;
-    var descripcion = document.getElementById("descripcion").value;
-    var semestre = document.getElementById("semestre").value;
-    var horasT = document.getElementById("horasT").value;
-    var horasC = document.getElementById("horasC").value;
+console.log("funciona")
+window.onload=function(){
 
-    var resultados = [materia, descripcion, semestre, horasT, horasC];
-    return resultados;
-}
-const printDatos = () => document.getElementById('resultados').innerHTML= resultados;
+var materia = document.getElementById('materia');
+var respuesta = document.getElementById('respuesta')
+
+materia.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log("click")
+    var datos = new FormData(materia);
+    console.log(datos.get('materia'))
+    console.log(datos.get('descripcion'))
+    console.log(datos.get('duracion'))
+    console.log(datos.get('horasT'))
+    console.log(datos.get('horasC'))
+
+    fetch('/post.php',{
+        method: 'POST',
+        body: datos
+    })
+        .then( res => res.json())
+        .then( data => {
+            console.log(data)
+            if(data === 'error'){
+                respuesta.innerHTML = `
+                <div class="alert alert-danger" role="alert">
+                    Llena todos los campos
+                </div>
+                `
+            }else{
+                respuesta.innerHTML = `
+                <div class="alert alert-primary" role="alert">
+                    ${data}
+                </div>
+                `
+            }
+        })
+    }
+)}
---
+++ web-client/javascript/post.php
@@ -0,0 +1,13 @@
+<?php
+
+$materia = $_POST['materia'];
+$descripcion = $_POST['descripcion'];
+$duracion = $_POST['duracion'];
+$horasT = $_POST['horasT'];
+$horasC = $_POST['horasC'];
+
+if($materia === '' || $descripcion=== ''|| $duracion=== ''|| $horasT=== ''|| $horasC=== ''){
+    echo json_encode('error');
+}else{
+    echo json_encode('Correcto: <br>Materia:'.$materia.'<br>Descripcion:'.$descripcion.'<br>Duracion:'.$duracion.'<br>Horas Totales:'.$horasT'<br>Horas Cursadas:'.$horasC);
+}
