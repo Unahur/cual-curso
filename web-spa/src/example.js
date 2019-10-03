4f846bace929045f089f53838339f92920a3541c
Andrés Atencio <aatencio@manas.tech>
2019-10-03 01:15:48 -0300

    Mi primer ejemplo

---
+++ web-spa/src/example.js
@@ -0,0 +1,29 @@
+const React = require('react')
+
+class FirstExample extends React.Component {
+    constructor(props) {
+        super(props);
+        // el state incluye los aspectos dinámicos de la página
+        this.state = { texto: "Miren lo que va a pasar acá", tamanioFuente: "medium"}
+    }
+
+    render() {
+        const theStyle = { fontSize: this.state.tamanioFuente }
+        // esto es una expresión JSX, parece HTML pero no lo es
+        return (
+            <div>
+                <p><span style={ theStyle }>
+                    { this.state.texto }
+                </span></p>
+                <button onClick={() => this.changeTextAndFont()}>React magic</button>
+            </div>
+        )
+    }
+
+    // función que reacciona al evento de apretar el botón
+    changeTextAndFont() {
+        this.setState({ texto: "Hello React", tamanioFuente: "25px" })
+    }
+}
+
+export default FirstExample;
