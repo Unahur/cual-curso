9b6ad3f3a34ed8d5d376092782ca126e751c1e65
chamiamarillo <yamil.amarillo@gmail.com>
2019-10-14 19:12:20 -0300

    terminados todos los metodos html, armando lo complicado de la appi

---
+++ web-api/routes/estudianes.js
@@ -0,0 +1,68 @@
+var express = require("express");
+var router = express.Router();
+var models = require("../models");
+
+
+router.get("/", (req, res) => {
+  models.estudiante
+    .findAll({
+      attributes: ["dni", "nombre_apellido"]
+    })
+    .then(estudiante => res.send(estudiante))
+    .catch(() => res.sendStatus(500));
+});
+
+
+router.post("/", (req, res) => {
+  models.estudiante
+    .create({ dni: req.body.dni, nombre_apellido: req.body.nombre_apellido })
+    .then(estudiante => res.status(201).send({ dni: estudiante.dni, nombre_apellido: estudiante.nombre_apellido }))
+    .catch(() => res.sendStatus(500));
+});
+
+
+const findEestudiante = (id, { onSuccess, onNotFound, onError }) => {
+  models.estudiante
+    .findOne({
+      attributes: ["id","dni","nombre_apellido"], // sacar el id ya que no lo quiero mostrar...!!!!!
+      where: { id }
+    })
+    .then(estudiante => (estudiante ? onSuccess(estudiante) : onNotFound()))
+    .catch(() => onError());
+};
+
+router.get("/:id", (req, res) => {
+  findEestudiante(req.params.id, {
+    onSuccess: estudiante => res.send(estudiante),
+    onNotFound: () => res.sendStatus(404),
+    onError: () => res.sendStatus(500)
+  });
+});
+
+router.put("/:id", (req, res) => {
+  const onSuccess = estudiante =>
+    estudiante
+      .update({ dni: req.body.dni, nombre_apellido: req.body.nombre_apellido }, { fields: ["dni","nombre_apellido"] })
+      .then(() => res.sendStatus(200))
+      .catch(() => res.sendStatus(500));
+    findEestudiante(req.params.id, {
+    onSuccess,
+    onNotFound: () => res.sendStatus(404),
+    onError: () => res.sendStatus(500)
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  const onSuccess = estudiante =>
+    estudiante
+      .destroy()
+      .then(() => res.sendStatus(200))
+      .catch(() => res.sendStatus(500));
+    findEestudiante(req.params.id, {
+    onSuccess,
+    onNotFound: () => res.sendStatus(404),
+    onError: () => res.sendStatus(500)
+  });
+});
+
+module.exports = router;
---
+++ web-client/abmEstudiante/js/app_estudiante.js
@@ -0,0 +1,28 @@
+function load() {
+    var url = 'http://localhost:3001/estudiantes';
+    var formulario = document.getElementById('creacionEstudiante');
+    
+
+
+formulario.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    
+
+    var datos = new FormData(formulario);
+    var data = {
+        dni: datos.get('dni'),
+        nombre_apellido: datos.get('nombre_apellido')
+    }
+    fetch(url, {
+            method: 'POST', // or 'PUT'
+            body: JSON.stringify(data), // data can be `string` or {object}!
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => console.log('Success:', response))
+        .then(document.getElementById('creacionEstudiante').reset());
+
+});
+}
