0e9cefd01ca23f6546687c94e3dbf9a57936fefb
chamiamarillo <yamil.amarillo@gmail.com>
2019-10-14 19:11:35 -0300

    terminados todos los metodos html, armando lo complicado de la appi

--- web-api/app.js
+++ web-api/app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
-var examplesRouter = require('./routes/examples');
+// var examplesRouter = require('./routes/examples');
+var estudiantesRouter = require('./routes/estudianes');
 var cors = require('cors')
 
 var app = express();
@@ -26,7 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.use('/', indexRouter);
-app.use('/examples', examplesRouter);
+// app.use('/examples', examplesRouter);
+app.use('/estudiantes',estudiantesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
