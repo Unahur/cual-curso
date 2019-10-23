256c2eef7506ab2430a3bdc68f78ad55e61c05ec
DemonMurderer <agustin120000@gmail.com>
2019-10-23 02:37:45 -0300

    frontend iteracion 1 listo

--- web-client/javascript/materia-create.js
+++ web-client/javascript/materia-create.js
@@ -5,21 +5,47 @@ var materia = document.getElementById('materia');
 
 materia.addEventListener('submit', function(e){
     e.preventDefault();
-    console.log("click")
-    var datos = new FormData(materia);
-    console.log(datos.get('materia'))
-    console.log(datos.get('descripcion'))
-    console.log(datos.get('duracion'))
-    console.log(datos.get('horasT'))
-    console.log(datos.get('horasC'))
+    console.log("click");
+    var formData = readForm();
+    const request = new Request('https://localhost:3001/materia',
+        { method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+                body: JSON.stringify(formData)
+        });
 
-    fetch('http://localhost:3001/materia' ,{
-        method: 'POST',
-        body: datos
-    })
-        .then( res => res.json())
-        .then( data => {
-            console.log(JSON.stringify(data))
+    fetch(request)
+        .then(response => {
+            if (response.status === 201) {
+               
+               return response.json();
+            } else {
+                throw new Error('Something went wrong on api server!');
+            }
         })
-    }
-)}
+        .then(response => {
+            console.debug(response);
+        }).catch(error => {
+            console.error(error);
+        });
+    
+    resetForm();
+})}
+function readForm() {
+    var formData = {};
+
+    formData["nombreMateria"] = document.getElementById('nombreMateria').value;
+    formData["descripcion"] = document.getElementById('descripcion').value;
+    formData["duracion"] = document.getElementById('duracion').value;
+    formData["horasT"] = document.getElementById('horasT').value;
+    return formData;
+}
+
+function resetForm() {
+    document.getElementById('nombreMateria').value = "";
+    document.getElementById('descripcion').value = "";
+    document.getElementById('duracion').value = "";
+    document.getElementById('horasT').value = "";
+}
+
