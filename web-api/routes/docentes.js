08ad559fe8a5bd314a1136b39f3ad76f6e564833
NicolasdelaFuente <ni.delafuente@gmail.com>
2019-10-23 19:58:24 -0300

    routes/docentes --creado

---
+++ web-api/routes/docentes.js
@@ -0,0 +1,68 @@
+var express = require("express");
+var router = express.Router();
+var models = require("../models");
+​
+router.get("/", (req, res) => {
+  models.docente
+    .findAll({
+      attributes: ["id", "nombre", "apellido", "sexo", "fechaNacimiento"]
+    })
+    .then(docentes => res.send(docentes))
+    .catch(() => res.sendStatus(500));
+});
+​
+router.post("/", (req, res) => {
+  models.docente
+    .create({ name: req.body.name })
+    .then(docente => res.status(201).send({ id: docente.id }))
+    .catch(() => res.sendStatus(500));
+});
+​
+const findDocente = (id, { onSuccess, onNotFound, onError }) => {
+  models.docente
+    .findOne({
+      attributes: ["id", "name"],
+      where: { id }
+    })
+    .then(docente => (docente ? onSuccess(docente) : onNotFound()))
+    .catch(() => onError());
+};
+​
+router.get("/:id", (req, res) => {
+  findDocente(req.params.id, {
+    onSuccess: docente => res.send(docente),
+    onNotFound: () => res.sendStatus(404),
+    onError: () => res.sendStatus(500)
+  });
+});
+​
+router.put("/:id", (req, res) => {
+  const onSuccess = docente =>
+    docente
+      .update({ name: req.body.name }, { fields: ["name"] })
+      .then(() => res.sendStatus(200))
+      .catch(() => res.sendStatus(500));
+  findDocente(req.params.id, {
+    onSuccess,
+    onNotFound: () => res.sendStatus(404),
+    onError: () => res.sendStatus(500)
+  });
+});
+​
+router.delete("/:id", (req, res) => {
+  const onSuccess = docente =>
+    docente
+      .destroy()
+      .then(() => res.sendStatus(200))
+      .catch(() => res.sendStatus(500));
+  findDocente(req.params.id, {
+    onSuccess,
+    onNotFound: () => res.sendStatus(404),
+    onError: () => res.sendStatus(500)
+  });
+});
+​
+module.exports = router;
+
+
+
