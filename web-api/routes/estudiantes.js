06802e4c38eef99d871c1e40b2d02142a4d18e49
marianraven <mariangsdh@gmail.com>
2019-10-24 15:13:29 -0300

    subiendo modificaciones back,funcionando

--- web-api/routes/estudiantes.js
+++ web-api/routes/estudiantes.js
@@ -2,64 +2,67 @@ var express = require("express");
 var router = express.Router();
 var models = require("../models");
 
+
 router.get("/", (req, res) => {
-    models.estudiantes
-        .findAll({
-            attributes: ["dni", "nombre_apellido"]
-        })
-        .then(estudiantes => res.send(estudiantes))
-        .catch(() => res.sendStatus(500));
+  models.estudiante
+    .findAll({
+      attributes: ["id", "dni", "nombre_apellido"]
+    })
+    .then(estudiante => res.send(estudiante))
+    .catch(() => res.sendStatus(500));
 });
+
 
 router.post("/", (req, res) => {
-    models.estudiantes
-        .create({ dni: req.body.dni, nombre_apellido: req.body.nombre_apellido, })
-        .then(estudiantes => res.status(201).send({ dni: estudiantes.dni, nombre_apellido: estudiantes.nombre_apellido }))
-        .catch(() => res.sendStatus(500));
+  models.estudiante
+    .create({ dni: req.body.dni, nombre_apellido: req.body.nombre_apellido })
+    .then(estudiante => res.status(201).send({ dni: estudiante.dni, nombre_apellido: estudiante.nombre_apellido }))
+    .catch(() => res.sendStatus(500));
 });
 
-const findestudiante = (dni, { onSuccess, onNotFound, onError }) => {
-    models.estudiantes
-        .findOne({
-            attributes: ["dni", "nombre_apellido"],
-            where: { dni }
-        })
-        .then(estudiantes => (estudiantes ? onSuccess(estudiantes) : onNotFound()))
-        .catch(() => onError());
+
+const findEestudiante = (id, { onSuccess, onNotFound, onError }) => {
+  models.estudiante
+    .findOne({
+      attributes: ["id","dni","nombre_apellido"], // sacar el id ya que no lo quiero mostrar...!!!!!
+      where: { id }
+    })
+    .then(estudiante => (estudiante ? onSuccess(estudiante) : onNotFound()))
+    .catch(() => onError());
 };
 
-router.get("/:dni", (req, res) => {
-    findestudiante(req.params.dni, {
-        onSuccess: estudiantes => res.send(estudiantes),
-        onNotFound: () => res.sendStatus(404),
-        onError: () => res.sendStatus(500)
-    });
+router.get("/:id", (req, res) => {
+  findEestudiante(req.params.id, {
+    onSuccess: estudiante => res.send(estudiante),
+    onNotFound: () => res.sendStatus(404),
+    onError: () => res.sendStatus(500)
+  });
 });
 
-router.put("/:dni", (req, res) => {
-    const onSuccess = estudiantes =>
-        estudiantes
-        .update({ dni: req.body.dni, nombre_apellido: req.body.nombre_apellido }, { fields: ["dni","nombre_apellido"] })
-        .then(() => res.sendStatus(200))
-        .catch(() => res.sendStatus(500));
-    findestudiante(req.params.dni, {
-        onSuccess,
-        onNotFound: () => res.sendStatus(404),
-        onError: () => res.sendStatus(500)
-    });
+router.put("/:id", (req, res) => {
+  const onSuccess = estudiante =>
+    estudiante
+      .update({ dni: req.body.dni, nombre_apellido: req.body.nombre_apellido }, { fields: ["dni","nombre_apellido"] })
+      .then(() => res.sendStatus(200))
+      .catch(() => res.sendStatus(500));
+    findEestudiante(req.params.id, {
+    onSuccess,
+    onNotFound: () => res.sendStatus(404),
+    onError: () => res.sendStatus(500)
+  });
 });
 
-router.delete("/:dni", (req, res) => {
-    const onSuccess = estudiantes =>
-        estudiantes
-        .destroy()
-        .then(() => res.sendStatus(200))
-        .catch(() => res.sendStatus(500));
-    findestudiante(req.params.dni, {
-        onSuccess,
-        onNotFound: () => res.sendStatus(404),
-        onError: () => res.sendStatus(500)
-    });
+router.delete("/:id", (req, res) => {
+  const onSuccess = estudiante =>
+    estudiante
+      .destroy()
+      .then(() => res.sendStatus(200))
+      .catch(() => res.sendStatus(500));
+    findEestudiante(req.params.id, {
+    onSuccess,
+    onNotFound: () => res.sendStatus(404),
+    onError: () => res.sendStatus(500)
+  });
 });
 
 module.exports = router;
