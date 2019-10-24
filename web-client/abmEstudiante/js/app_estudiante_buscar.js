6b2a12032ceec17621f92cf9ba6c0c0115b3184c
adramarilla <adriana.amarilla@unahur.edu.ar>
2019-10-24 19:46:17 -0300

    funcionando GET

--- web-client/abmEstudiante/js/app_estudiante_buscar.js
+++ web-client/abmEstudiante/js/app_estudiante_buscar.js
@@ -1,48 +1,21 @@
-    var url = 'http://localhost:3001/estudiantes';
-    var botonBuscar = document.getElementById('buscar');
+var url = 'http://localhost:3001/estudiantes';
+    //var formulario = document.getElementById('buscarPersona');
     //var persona = document.getElementsByClassName('modal-content');
-    
 
+    document.getElementById('buscarDni').addEventListener('click', function(){
+        console.log(" Entro al evento ");
+   
+    fetch(url, 
+        {
+            method: 'GET' 
+        }
+        ).then(res => res.json())
+       .then(dato => formatearDato(dato)) 
+       .catch(error => console.error('Error:', error))
+       .then(response => console.log('Success:', response));
+//      .then(document.getElementById('creacionEstudiante').reset());
+    });
 
-    botonBuscar.addEventListener('submit', function (evt) {
-    evt.preventDefault();
-
-    var datos = new FormData(botonBuscar);
-    var data = {
-        id: datos.get('id'),
-        dni: datos.get('dni'),
-        nombre_apellido: datos.get('nombre_apellido')
+    function formatearDato(dato) {
+        console.log(dato);
     }
-
-    
-    fetch(url , {
-            method: 'GET', 
-            body: JSON.stringify(data), 
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(res => res.json())
-        .catch(error => console.error('Error:', error))
-        .then(response => console.log('Success:', response))
-
-  //      .then(document.getElementById('creacionEstudiante').reset());
-
-});
-
-
-/*
-formulario.addEventListener('submit', function (evt) {
-    evt.preventDefault();
-    var validar =confirm("Esta seguro de borrar su registro");
-        if(validar){
-fetch(url + "/" + dni, {
-    method: 'DELETE'
-  }).then(() => {
-     console.log('removed dni '+  dni);
-  }).catch(err => {
-    console.error(err)
-  });
-
-  } });
-*/
-
