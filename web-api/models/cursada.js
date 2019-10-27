85a9b257bdf7e40d4f9f8da3ef08e582b86132b3
Adrian Fazio <adrian.fazio@outlook.com>
2019-10-27 11:46:38 -0300

    Segunda Iteraccion mejorada

--- web-api/models/cursada.js
+++ web-api/models/cursada.js
@@ -5,6 +5,11 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Cursada.associate = function(models) {
     // associations can be defined here
-  };
+    Cursada.belongsToMany(models.docente, {
+      through: 'DocenteCursadas',
+      as: 'docente',
+      foreignKey: 'cursadaId',
+      otherKey: 'docenteId'
+    });
   return Cursada;
-};
+}};
