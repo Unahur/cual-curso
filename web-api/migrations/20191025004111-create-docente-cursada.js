30263320f5710d4343dc79132d86ff68e0b60a59
Nicolas <ni.delafuente@gmail.com>
2019-10-30 12:52:25 -0300

    allowNull: false,

--- web-api/migrations/20191025004111-create-docente-cursada.js
+++ web-api/migrations/20191025004111-create-docente-cursada.js
@@ -9,6 +9,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       docenteId: {
+        allowNull: false,
         type: Sequelize.INTEGER,
         onDelete: "CASCADE",
         references: {
@@ -17,6 +18,7 @@ module.exports = {
         }
       },
       cursadaId: {
+        allowNull: false,
         type: Sequelize.INTEGER,
         onDelete: "CASCADE",
         references: {
