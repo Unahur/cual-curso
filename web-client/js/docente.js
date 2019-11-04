866e377f89ab3c66e743cad3258b4e923bce6cfd
Nicolas <ni.delafuente@gmail.com>
2019-11-04 12:08:17 -0300

    GET OK

---
+++ web-client/js/docente.js
@@ -0,0 +1,8 @@
+const http = 'http://localhost:3001/docentes';
+
+var http=
+form = require('fs').readFileSync()
+
+function webServer(req, res)
+
+console.log("Servidor corriendo en http://localhost:3001/")
--- web-client/js/formulario.js
+++ web-client/js/formulario.js
@@ -1,13 +1,25 @@
 'use strict'
 
-var inputs = document.getElementsByClassName('formularioInput');
+var url = 'http://localhost:3001/docentes';
+var formulario = document.getElementById('formulario');
 
-for (var i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener('keyup', function(){
-        if(this.value.length >= 1) {
-            this.nextElementSibling.classList.add('fijarLabel');
-        }else{
-            this.nextElementSibling.classList.remove('fijarLabel');
-        }
-    })
+formulario.addEventListener('submit', function (evt) {
+evt.preventDefault();
+
+var datos = new FormData(formulario);
+var data = {
+    nombre: datos.get('nombre'),
+    apellido: datos.get('apellido'),
+    dni: datos.get('dni'),    
 }
+fetch(url, {
+        method: 'POST', 
+        body: JSON.stringify(data), 
+        headers: { 'Content-Type': 'application/json'  }
+    })
+    .then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => console.log('Success:', response))
+    .then(document.getElementById('formulario').reset());
+
+});
