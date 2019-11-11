ae218e79099c737b8edac6bddd42221f752776b4
Agustin Remonda <42420863+Agustin12000@users.noreply.github.com>
2019-11-11 15:46:40 -0300

    get materia, con correlativa y paginacion

--- web-api/routes/materia.js
+++ web-api/routes/materia.js
@@ -2,14 +2,29 @@ var express = require("express");
 var router = express.Router();
 var models = require("../models");
 
-router.get("/",(req,res)=>{
+router.get("/", (req,res)=>{
     models.materia
     .findAll({
-        attributes:["id","name","description","duration", "totalHours"]
+        attributes:["id","name","description","duration", "totalHours", "correlativa_id"],
     })
     .then(materias=>res.send(materias))
     .catch(()=>res.sendStatus(500));
 });
+
+router.get("/paginacion/:index", (req,res) =>{
+    let page=req.params.index;
+    const pageSize=4;
+    const offset = page * pageSize;
+    const limit = offset + pageSize;
+    models.materia
+    .findAll({
+        attributes:["id","name","description","duration", "totalHours", "correlativa_id"],
+        offset,
+        limit
+    })
+    .then(materias=>res.send(materias))
+    .catch(()=>res.sendStatus(500));
+})
 
 router.post("/",(req,res)=>{
     models.materia
@@ -21,7 +36,7 @@ router.post("/",(req,res)=>{
 const findMateria = (id,{onSuccess,onNotFound,onError})=>{
     models.materia
     .findOne({
-    attributes:["id","name","description","duration","totalHours"],
+    attributes:["id","name","description","duration","totalHours", "correlativa_id"],
     where:{ id }
     })
     .then(materia => (materia ? onSuccess(materia):onNotFound()))
@@ -60,6 +75,8 @@ router.delete("/:id",(req,res) =>{
         onError: () => res.sendStatus(500)
     });
 });
+
+module.exports = router;
 
 module.exports = router;
 
